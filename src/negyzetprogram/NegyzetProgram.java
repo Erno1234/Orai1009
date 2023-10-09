5565273d2f8672ff014af3f0ed4947373a298054
sw4q3ar2 <biggercsgo@gmail.com>
2023-10-09 13:12:24 +0200

    1. feladat

--- src/negyzetprogram/NegyzetProgram.java
+++ src/negyzetprogram/NegyzetProgram.java
@@ -2,9 +2,17 @@
 package negyzetprogram;
 
 public class NegyzetProgram {
-
+       
     public static void main(String[] args) {
-        
+        new NegyzetProgram().feladat();
     }
     
+    private void feladat(){
+        negyzet n = new negyzet();
+        n.setOldal(2);
+        n.setX(20);
+        n.setY(30);
+        
+        System.out.println(n.toString());
+    }
 }
--- src/negyzetprogram/negyzet.java
+++ src/negyzetprogram/negyzet.java
@@ -2,5 +2,41 @@
 package negyzetprogram;
 
 public class negyzet {
+    private double x;
+    private double y;
+    private double oldal;
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setOldal(double oldal) {
+        this.oldal = oldal;
+    }
+
+    public double getOldal() {
+        return oldal;
+    }
+
+    public double terulet() {
+        return oldal * oldal;
+    }
+
+    @Override
+    public String toString() {
+        return "negyzet{" + "x=" + x + ", y=" + y + ", oldal=" + oldal + ", területe=" + terulet() + '}';
+    }
     
 }
