104a89cec8de17d227bba127784ed8414fac7a79
Erno1234 <palocierno12@gmail.com>
2023-10-09 13:14:51 +0200

    getterek

--- src/negyzetprogram/NegyzetProgram.java
+++ src/negyzetprogram/NegyzetProgram.java
@@ -2,9 +2,10 @@
 package negyzetprogram;
 
 public class NegyzetProgram {
-
+    private static negyzet n1 = new negyzet();
+    
     public static void main(String[] args) {
-        
+        System.out.println("Négyzet oldal hossza: "+ n1.getOldalhossz() +" X koordináta: " +n1.getX() +" Y koordináta: " +n1.getY());
     }
     
 }
--- src/negyzetprogram/negyzet.java
+++ src/negyzetprogram/negyzet.java
@@ -2,5 +2,26 @@
 package negyzetprogram;
 
 public class negyzet {
+    private int oldal;
+    private double x;
+    private double y;
+    
+    
+    public int getOldalhossz(){
+        return oldal;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+    
+    public double terulete(){
+        return Math.pow(this.oldal, 2);
+    }
+    
     
 }
